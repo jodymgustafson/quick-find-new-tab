45f3d8fb9c8150dcfa849986a82881704445cf85
Jody Gustafson <jgustafson@hagerty.com>
2018-08-07 13:19:21 -0400

    Updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -5,14 +5,19 @@ var uglify = require('gulp-uglify');
 var replace = require('gulp-replace');
 var del = require("del");
 
-var webpackConfig = {
+var webpackConfigApp = {
     output: {
         filename: 'app.js',
     }
 };
+var webpackConfigOpt = {
+    output: {
+        filename: 'options.js',
+    }
+};
 
 // Deletes the compiled js files
-gulp.task('clean', function() {
+gulp.task('clean', () => {
     return del(
         ['src/**/*.js.map', 'src/*.js'],
         { force: true }
@@ -20,24 +25,29 @@ gulp.task('clean', function() {
 });
 
 // Deletes the contents of the dist folder
-gulp.task('clean-dist', function() {
+gulp.task('clean-dist', () => {
     return del('dist/**/*', { force: true });
 });
 
-// Combines and minifies the js for release
-gulp.task('package', ['clean-dist'], function () {
-    gulp.src('src/options.js')
-        .pipe(webpack(webpackConfig))
-        .pipe(uglify())
-        .pipe(gulp.dest('dist/'));
+gulp.task('package-app', ['clean-dist'], () => {
     return gulp.src('src/app.js')
-        .pipe(webpack(webpackConfig))
-        .pipe(uglify())
+        .pipe(webpack(webpackConfigApp))
+        //.pipe(uglify())
         .pipe(gulp.dest('dist/'));
 });
 
+gulp.task('package-opts', ['clean-dist'], () => {
+    return gulp.src('src/options.js')
+        .pipe(webpack(webpackConfigOpt))
+        //.pipe(uglify())
+        .pipe(gulp.dest('dist/'));
+});
+
+// Combines and minifies the js for release
+gulp.task('package', ['clean-dist', 'package-app', 'package-opts']);
+
 // Copies all supporting files to dist
-gulp.task('copy-files', ['clean-dist', 'package'], function () {
+gulp.task('copy-files', ['clean-dist', 'package'], () => {
     function copyFilesToDist(path, file) {
         return gulp.src('src/' + path + file)
                 //.pipe(debug({ title: "Copying " }))
@@ -45,13 +55,18 @@ gulp.task('copy-files', ['clean-dist', 'package'], function () {
     }
     
     return merge(
-        copyFilesToDist('scripts/', '*.js'),
+        //copyFilesToDist('scripts/', '*.js'),
         copyFilesToDist('images/', '*.*'),
-        copyFilesToDist('./', 'app.min.css'),
+        copyFilesToDist('./', '*.min.css'),
+        copyFilesToDist('./', 'manifest.json'),
+        copyFilesToDist('./', 'background.js'),
         gulp.src('src/index.html')
             // Inject version and update app.js script tag
-            .pipe(replace('<script src="scripts/require.js" data-main="./app"></script>', '<script src="app.js?{version}></script>'))
-            .pipe(replace('{version}', 'v=' + new Date().getTime()))
+            .pipe(replace('<script src="scripts/require.js" data-main="./app"></script>', `<script src="app.js?v=${new Date().getTime()}"></script>`))
+            .pipe(gulp.dest('dist/')),
+        gulp.src('src/options.html')
+            // Inject version and update options.js script tag
+            .pipe(replace('<script src="scripts/require.js" data-main="./options"></script>', `<script src="options.js?v=${new Date().getTime()}"></script>`))
             .pipe(gulp.dest('dist/'))
         );
 });
